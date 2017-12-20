a53342b0b5cc2fef5e61cd5669b0247c3376db52
AkioFaris <rnanimo@gmail.com>
2017-12-20 14:24:31 +0300

    Add logger, assertion messages and add search in document by tag name

--- src/main/java/utils/DomHandler.java
+++ src/main/java/utils/DomHandler.java
@@ -10,7 +10,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
 public class DomHandler {
-	public static String getTagTextContent(Document document, String tagXpath) throws XPathExpressionException {
+	public static String getTagTextByXpath(Document document, String tagXpath) throws XPathExpressionException {
 		XPathFactory pathFactory = XPathFactory.newInstance();
 		XPath xpath = pathFactory.newXPath();
 		XPathExpression expr = xpath.compile(tagXpath);
@@ -19,12 +19,16 @@ public class DomHandler {
 			return "";
 		return nodes.item(0).getTextContent();
 	}
+	
+	public static String getTagTextByTagName(Document document, String tagName){
+		NodeList nodes = document.getElementsByTagName(tagName);
+		if (nodes.getLength() == 0)
+			return "";
+		return nodes.item(0).getTextContent();
+	}
 
-	public static int countTagOccurrences(Document document, String tagXpath) throws XPathExpressionException {
-		XPathFactory pathFactory = XPathFactory.newInstance();
-		XPath xpath = pathFactory.newXPath();
-		XPathExpression expr = xpath.compile(tagXpath);
-		NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+	public static int countTagOccurrences(Document document, String tagName){
+		NodeList nodes = document.getElementsByTagName(tagName);
 		return nodes.getLength();
 	}
 }
--- src/test/java/xml/parsing/DomParsingTest.java
+++ src/test/java/xml/parsing/DomParsingTest.java
@@ -1,7 +1,8 @@
 package xml.parsing;
 
 import static utils.DomHandler.countTagOccurrences;
-import static utils.DomHandler.getTagTextContent;
+import static utils.DomHandler.getTagTextByTagName;
+import static utils.DomHandler.getTagTextByXpath;
 
 import java.io.IOException;
 
@@ -10,6 +11,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,36 +21,37 @@ import org.xml.sax.SAXException;
 
 public class DomParsingTest {
 	private Document document = null;
+	final Logger logger = LoggerFactory.getLogger(DomParsingTest.class);
 
 	@BeforeMethod
 	public void createDom() throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 		document = documentBuilder.parse("src\\test\\resources\\document.xml");
+		Assert.assertNotNull(document, "The document was not parsed successfully: ");
 	}
 
 	@Test
-	public void checkCoutryTag() throws XPathExpressionException {
-		Assert.assertNotNull(document);
-
-		String country = getTagTextContent(document, "//country");
-		Assert.assertEquals(country, "HONG_KONG");
+	public void checkCoutryTag() {
+		String tagName = "country";
+		String expectedText = "HONG_KONG";
+		String tagText = getTagTextByTagName(document, tagName);
+		Assert.assertEquals(tagText, expectedText, 
+				"The text of '" + tagName + "' tag isn't equal to " + expectedText + ": ");
 	}
 
 	@Test
 	public void checkParagrapthTitle() throws XPathExpressionException {
-		Assert.assertNotNull(document);
-
-		String title = getTagTextContent(document, "//paragraph[@id='593ASECT1']/title");
-		Assert.assertTrue(title.contains("Section"));
+		String expectedText = "Section";
+		String titleText = getTagTextByXpath(document, "//paragraph[@id='593ASECT1']/title");
+		Assert.assertTrue(titleText.contains(expectedText), 
+				"Paragraph title's text doesn't contain '" + expectedText + "': ");
 	}
 
 	@Test
-	public void checkHeadingTagNumber() throws XPathExpressionException {
-		Assert.assertNotNull(document);
-		
-		int numOfOccurr = countTagOccurrences(document, "//heading");
-		Assert.assertTrue(numOfOccurr > 1);
-		System.out.println("Number of tag 'heading' occurrences: " + numOfOccurr);
+	public void checkHeadingTagNumber() {
+		String tagName = "heading";
+		int numOfOccurr = countTagOccurrences(document, tagName);
+		Assert.assertTrue(numOfOccurr > 1, "Tag '" + tagName + "' is absent in the document.: ");
+		logger.info("Number of tag 'heading' occurrences: " + numOfOccurr);
 	}
-
 }
